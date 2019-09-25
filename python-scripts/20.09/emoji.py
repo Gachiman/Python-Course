68416657cce9d29ad314143802c34e7095634028
Ilya Aglikov <aglikov@bk.ru>
2019-09-25 13:13:39 +0300

    emoji task done

---
+++ python-scripts/20.09/emoji.py
@@ -0,0 +1,13 @@
+old_smile = {':-)': '\U0001f642', '}:-)': '\U0001f608', ':-(': '\U00002639', ':-|': '\U0001f610', '=_=': '\U0001f611',
+             'O_o': '\U0001f914', ':-O': '\U0001f632', ':-\\': '\U0001f615', '*\\O/*': '\U0001f483'}
+with open("emoji_text.txt", "r", encoding='utf-8-sig') as infile,\
+        open("emoji_text_final.txt", "w", encoding='utf-8-sig') as outfile:
+    for line in infile:
+        words = line.split()
+        for item in words:
+            if item in old_smile:
+                words[words.index(item)] = old_smile[item]
+        print(words)
+        lol = " ".join(words)
+        print(lol)
+        outfile.write(lol)
