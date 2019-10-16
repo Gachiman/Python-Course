980801fa1ba63013328458c56ca375c08ba1fd44
Ilya Aglikov <aglikov@bk.ru>
2019-10-16 19:48:41 +0300

    Emoji:re

--- python-scripts/20.09/emoji.py
+++ python-scripts/20.09/emoji.py
@@ -1,8 +1,11 @@
+import re
+
+
 old_smile = {':-)': '\U0001f642', '}:-)': '\U0001f608', ':-(': '\U00002639', ':-|': '\U0001f610', '=_=': '\U0001f611',
              'O_o': '\U0001f914', ':-O': '\U0001f632', ':-\\': '\U0001f615', '*\\O/*': '\U0001f483'}
 with open("emoji_text.txt", "r", encoding='utf-8-sig') as infile,\
         open("emoji_text_final.txt", "w", encoding='utf-8-sig') as outfile:
-    for line in infile:
+    '''for line in infile:
         words = line.split()
         for item in words:
             if item in old_smile:
@@ -10,4 +13,7 @@ with open("emoji_text.txt", "r", encoding='utf-8-sig') as infile,\
         print(words)
         lol = " ".join(words)
         print(lol)
-        outfile.write(lol)
+        outfile.write(lol)'''
+    for line in infile:
+        outfile.write(re.sub(r'[-}:)(|O_o\\*/=]{3,5}', old_smile[re.search(r'[-}:)(|O_o\\*/=]{3,5}',
+                                                                           line).group()], line))
